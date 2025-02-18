d62562dd5bc36a6e6cbef2fa787f938613e0ac2f
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-02-18 11:32:34 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,24 +1,22 @@
-import java.util.*;
-public class hello
-  {
-    static int add(int a , int b)
-    {
-      return a+b;
+public class hello {
+    static int add(int a, int b) {
+        return a + b;
     }
 
-        static int subtract(int a , int b)
-    {
-      return a-b;
+    static int subtract(int a, int b) {
+        return a - b; // Corrected the subtraction logic
     }
-  public static void main(String[] args)
-    {
-    // System.out.println("this is run in jenkine from the github repo");
-    Scanner in = new Scanner(System.in);
-    int  a = in.nextInt();
-    int  b = in.nextInt();
-     System.out.println("Addition Of a = "+a +" b = " +b +" is " +add(a,b) ) ;
-           System.out.println("subtraction Of a = "+a +" b = " +b +" is " +subtract(a,b) ) ;
-      
-      
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Please provide two integer arguments.");
+            return;
+        }
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("Addition of a = " + a + " and b = " + b + " is " + add(a, b));
+        System.out.println("Subtraction of a = " + a + " and b = " + b + " is " + subtract(a, b));
     }
-  }
+}
